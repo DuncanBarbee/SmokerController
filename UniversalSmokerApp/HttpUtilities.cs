9edb6915a24e303ab9b56678b1e3364606492647
bbaffy <bill@luckyjumpgames.com>
2016-03-30 00:42:31 -0400

    Initial commit of universal app

---
+++ UniversalSmokerApp/HttpUtilities.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using System.Net;
+
+// For trace
+using System.Diagnostics;
+
+namespace UniversalSmokerApp
+{
+    class HttpUtilities
+    {
+        static int buffSize = 1024;
+        static int stringBuilderSize = 4096 * 4;
+
+        public static async System.Threading.Tasks.Task<string> HttpGet(string RequestUrl)
+        {
+            string Doc = null;
+
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(RequestUrl);
+                req.ContentType = "text/html";
+                req.Method = "GET";
+                //req.ProtocolVersion = System.Net.HttpVersion.Version10;
+                //req.KeepAlive = false;
+                //req.ServicePoint.Expect100Continue = false; 
+
+                WebResponse res = await req.GetResponseAsync();
+                if (req.HaveResponse && (res.GetResponseStream() != null))
+                {
+                    // Sometimes the stream reader 'stalls' when we try to tokenize while reading 
+                    //  the stream and only returns the partial result, so need to read the entire
+                    //  response stream before passing it to the JSON parser
+                    //TextReader str = new StreamReader(res.GetResponseStream(), Encoding.UTF8);
+                    //jDoc = new JSONParser.JSONDoc(str);
+
+                    // read the entire response buffer into a string
+                    if (res.ContentLength > 0)
+                    {
+                        // the easy way, we know how much we need to read
+                        byte[] buff;
+                        buff = new byte[res.ContentLength];
+                        int cnt = 0;
+                        while (cnt < (int)res.ContentLength)
+                        {
+                            int bytesLeft = (int)res.ContentLength - cnt;
+                            cnt += res.GetResponseStream().Read(buff, cnt, bytesLeft);
+                        }
+
+                        Doc = Encoding.UTF8.GetString(buff);
+                    }
+                    else
+                    {
+                        // we didn't get a length, so read the response in chunks and dynamically build the string
+                        // the easy way, we know how much we need to read
+                        byte[] buff = new byte[buffSize];
+
+                        StringBuilder sb = new StringBuilder(stringBuilderSize);
+                        int bytesRead = 1; // trick it first time through
+                        while (bytesRead > 0)
+                        {
+                            bytesRead = res.GetResponseStream().Read(buff, 0, buffSize);
+                            if (bytesRead > 0)
+                            {
+                                sb.Append(Encoding.UTF8.GetString(buff, 0, bytesRead));
+                            }
+                        }
+                        Doc = sb.ToString();
+                    }
+                }
+                //res.Dispose();
+
+                return Doc;
+            }
+            catch (WebException ex)
+            {
+                if ((ex.Response != null) && (ex.Response.GetResponseStream() != null))
+                {
+                    if (ex.Response.ContentLength > 0)
+                    {
+                        // the easy way, we know how much we need to read
+                        byte[] buff;
+                        buff = new byte[ex.Response.ContentLength];
+                        int cnt = 0;
+                        while (cnt < (int)ex.Response.ContentLength)
+                        {
+                            int bytesLeft = (int)ex.Response.ContentLength - cnt;
+                            cnt += ex.Response.GetResponseStream().Read(buff, cnt, bytesLeft);
+                        }
+
+                        string str = Encoding.UTF8.GetString(buff);
+
+                        throw new Exception(str, ex);
+                    }
+                    else
+                    {
+                        TextReader str = new StreamReader(ex.Response.GetResponseStream(), Encoding.UTF8);
+                        Doc = str.ReadToEnd();
+                        ex.Response.Dispose();
+
+                        throw new Exception(Doc.ToString(), ex);
+                    }
+                }
+                //Trace.WriteLine(string.Format("Exception in SwarmServer.HttpPost: {0}", ex.Message));
+                //Trace.WriteLine(ex.StackTrace);
+
+                throw new Exception(ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                //Trace.WriteLine(string.Format("Exception in SwarmServer.HttpPost: {0}", ex.Message));
+                //Trace.WriteLine(ex.StackTrace);
+
+                throw;
+            }
+        }
+
+        public static async System.Threading.Tasks.Task<string> HttpPost(string RequestUrl, string RequestContent)
+        {
+            string Doc = null;
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(RequestUrl);
+
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.Method = "POST";
+
+            try
+            {
+                if (RequestContent != null)
+                {
+                    byte[] bytedata = Encoding.UTF8.GetBytes(RequestContent);
+                    //req.ContentLength = bytedata.Length;
+                    //req.GetRequestStream().Write(bytedata, 0, bytedata.Length);
+                    Doc = "this shit was commented out";
+                }
+                WebResponse res = await req.GetResponseAsync();
+
+                if (req.HaveResponse && (res != null) && (res.GetResponseStream() != null))
+                {
+                    if (res.ContentLength > 0)
+                    {
+                        // the easy way, we know how much we need to read
+                        byte[] buff;
+                        buff = new byte[res.ContentLength];
+                        int cnt = 0;
+                        while (cnt < (int)res.ContentLength)
+                        {
+                            int bytesLeft = (int)res.ContentLength - cnt;
+                            cnt += res.GetResponseStream().Read(buff, cnt, bytesLeft);
+                        }
+
+                        Doc = Encoding.UTF8.GetString(buff);
+                    }
+                    else
+                    {
+                        TextReader str = new StreamReader(res.GetResponseStream(), Encoding.UTF8);
+                        Doc = str.ReadToEnd();
+                    }
+                }
+                res.Dispose();
+
+                return Doc;
+            }
+            catch (WebException ex)
+            {
+                if ((ex.Response != null) && (ex.Response.GetResponseStream() != null))
+                {
+                    if (ex.Response.ContentLength > 0)
+                    {
+                        // the easy way, we know how much we need to read
+                        byte[] buff;
+                        buff = new byte[ex.Response.ContentLength];
+                        int cnt = 0;
+                        while (cnt < (int)ex.Response.ContentLength)
+                        {
+                            int bytesLeft = (int)ex.Response.ContentLength - cnt;
+                            cnt += ex.Response.GetResponseStream().Read(buff, cnt, bytesLeft);
+                        }
+
+                        Doc = Encoding.UTF8.GetString(buff);
+
+                        throw new Exception(Doc, ex);
+                    }
+                    else
+                    {
+                        TextReader str = new StreamReader(ex.Response.GetResponseStream(), Encoding.UTF8);
+                        Doc = str.ReadToEnd();
+                        ex.Response.Dispose();
+
+                        throw new Exception(Doc);
+                    }
+                }
+                //Trace.WriteLine(string.Format("Exception in SwarmServer.HttpPost: {0}", ex.Message));
+                //Trace.WriteLine(ex.StackTrace);
+
+                throw new Exception(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                //Trace.WriteLine(string.Format("Exception in SwarmServer.HttpPost: {0}", ex.Message));
+                //Trace.WriteLine(ex.StackTrace);
+
+                throw;
+            }
+        }
+    }
+}
---
+++ UniversalSmokerApp/MainPage.xaml.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UniversalSmokerApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private async void Get_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string status = "";
+            try
+            {
+                status = await HttpUtilities.HttpGet(IPInput.Text) + "\r\n";
+            }
+            catch
+            {
+                var dialog = new Windows.UI.Popups.MessageDialog("Invalid Smoker IP Address");
+                await dialog.ShowAsync();
+                return;
+            }
+
+            string[] items =   status.Split(new string[] { "</td></tr>" },StringSplitOptions.None);
+            string smokerTemp = items[0].Substring(items[0].LastIndexOf("<td>")+4);
+            Dictionary<string, string> probes = new Dictionary<string, string>();
+            for (int i=1; i < items.Count(); i++)
+            {
+                if(items[i].Contains("Probe"))
+                {
+                    string probeInfo = items[i].Substring(items[i].IndexOf("P"));
+                    string[] probeData = probeInfo.Split(new string[]
+                        { "</td><td>" }, StringSplitOptions.None);
+                    probes.Add(probeData[0], probeData[1]);
+                }
+                if (items[i].Contains("Target Temperature"))
+                {
+                    string targetTemp = items[i].Substring(items[i].IndexOf("value") + 7);
+                    targetTemp = targetTemp.Remove(targetTemp.IndexOf("'"));
+                    tempInput.PlaceholderText = targetTemp;
+                    int target = 0;
+
+                    Int32.TryParse(targetTemp, out target);
+                    smokerTempData.Content = smokerTemp;
+
+                    smokerTemp = smokerTemp.Remove(smokerTemp.IndexOf(' '));
+                    int tempActual = 0;
+                    Int32.TryParse(smokerTemp, out tempActual);
+
+                    if (target > tempActual + 5)
+                    {
+                        smokerTempData.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+                        smokerTempLbl.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+                    }
+                    if (target >= tempActual - 5 && target <= tempActual + 5)
+                    {
+                        smokerTempData.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+                        smokerTempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+                    }
+                    if (target < tempActual - 5)
+                    {
+                        smokerTempData.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                        smokerTempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                    }
+                }
+            }
+
+            string data;
+            int temp = 0;
+
+            probes.TryGetValue("Probe 1", out data);
+            if (data == null)
+            { data = "disconnected"; temp = 0; probe1TempData.Content = data;
+            }
+            else
+            {
+                probe1TempData.Content = data;
+                data = data.Remove(data.IndexOf(' '));
+                Int32.TryParse(data, out temp);
+            }
+            if (temp < 190)
+            {
+                probe1TempData.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+                probe1TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+            }
+            if (temp >= 190 && temp <= 205)
+            {
+                probe1TempData.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+                probe1TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+            }
+            if (temp > 205)
+            {
+                probe1TempData.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                probe1TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+
+            probes.TryGetValue("Probe 2", out data);
+            if (data == null)
+            { data = "disconnected"; temp = 0; probe2TempData.Content = data;
+            }
+            else
+            {
+                probe2TempData.Content = data;
+                data = data.Remove(data.IndexOf(' '));
+                Int32.TryParse(data, out temp);
+            }
+            if (temp < 190)
+            {
+                probe2TempData.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+                probe2TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+            }
+            if (temp >= 190 && temp <= 205)
+            {
+                probe2TempData.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+                probe2TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+            }
+            if (temp > 205)
+            {
+                probe2TempData.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                probe2TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+
+            probes.TryGetValue("Probe 3", out data);
+            if (data == null)
+            { data = "disconnected"; temp = 0; probe3TempData.Content = data;
+            }
+            else
+            {
+                probe3TempData.Content = data;
+                data = data.Remove(data.IndexOf(' '));
+                Int32.TryParse(data, out temp);
+            }
+            if (temp < 190)
+            {
+                probe3TempData.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+                probe3TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+            }
+            if (temp >= 190 && temp <= 205)
+            {
+                probe3TempData.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+                probe3TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+            }
+            if (temp > 205)
+            {
+                probe3TempData.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                probe3TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+
+            probes.TryGetValue("Probe 4", out data);
+            if (data == null)
+            { data = "disconnected"; temp = 0; probe4TempData.Content = data;
+            }
+            else
+            {
+                probe4TempData.Content = data;
+                data = data.Remove(data.IndexOf(' '));
+                Int32.TryParse(data, out temp);
+            }
+            if (temp < 190)
+            {
+                probe4TempData.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+                probe4TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+            }
+            if (temp >= 190 && temp <= 205)
+            {
+                probe4TempData.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+                probe4TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+            }
+            if (temp > 205)
+            {
+                probe4TempData.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                probe4TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+
+            probes.TryGetValue("Probe 5", out data);
+            if (data == null)
+            { data = "disconnected"; temp = 0; probe5TempData.Content = data;
+            }
+            else
+            {
+                probe5TempData.Content = data;
+                data = data.Remove(data.IndexOf(' '));
+                Int32.TryParse(data, out temp);
+            }
+            if (temp < 190)
+            {
+                probe5TempData.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+                probe5TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+            }
+            if (temp >= 190 && temp <= 205)
+            {
+                probe5TempData.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+                probe5TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+            }
+            if (temp > 205)
+            {
+                probe5TempData.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                probe5TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+
+            probes.TryGetValue("Probe 6", out data);
+            if (data == null)
+            { data = "disconnected"; temp = 0; probe6TempData.Content = data;
+            }
+            else
+            {
+                probe6TempData.Content = data;
+                data = data.Remove(data.IndexOf(' '));
+                Int32.TryParse(data, out temp);
+            }
+            if (temp < 190)
+            {
+                probe6TempData.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+                probe6TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.DodgerBlue);
+            }
+            if (temp >= 190 && temp <= 205)
+            {
+                probe6TempData.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+                probe6TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Lime);
+            }
+            if (temp > 205)
+            {
+                probe6TempLbl.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                probe6TempData.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+        }
+
+        private async void Post_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string status = await HttpUtilities.HttpGet(string.Format("{0}?t={1}",
+                    IPInput.Text, tempInput.Text));
+            }
+            catch
+            {
+                var dialog = new Windows.UI.Popups.MessageDialog("Invalid Smoker IP Address");
+                await dialog.ShowAsync();
+                return;
+            }
+            Get_Button_Click(sender, e);
+        }
+    }
+}
